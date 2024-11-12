9650abc4a20c4c1061921d86768207a49cacd8a2
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2024-11-12 13:24:45 +0100

    added reset function

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -31,15 +31,38 @@ this.selectsound.src = selectsound
         this.game.keys.push(event.key)
       }
 
+      // Player2 shooting
       if (event.key === ' ') {
         this.punchsound.play()
         this.game.player2.setState("shooting")
       }
 
+      // Activate debug mode
       if (event.key === 'p') {
         this.game.debug = !this.game.debug
       }
 
+      // For restarting the game
+      if (event.key === 'r' && this.game.gameOver || this.game.gameWin) {
+        this.game.gameOver = false
+        this.game.gameStart = false
+        this.game.gameWin = false
+        this.game.viewMainMenu = true
+        this.game.player.powerState = false
+        this.game.player2.powerStateP2 = false
+        this.game.player.lives = 7
+        this.game.player2.lives = 7
+        this.game.enemiesKilled = 0
+        this.game.player.ammo = 20
+        this.game.HealthBar.frameX = 0
+        this.game.HealthBarP2.frameX = 0
+        this.game.player.x = 100
+        this.game.player.y = 100
+        this.game.player2.x = 500
+        this.game.player2.y = 500
+      }
+
+      // For starting the game
       if (event.key === 'g') {
         this.selectsound.play()
         this.game.viewControls = false
@@ -48,6 +71,8 @@ this.selectsound.src = selectsound
         this.game.gameStart = true
         console.log('start game')
       }
+
+      // For viewing the credits
       if (event.key === 'c' && this.game.gameStart === false) {
         this.selectsound.play()
         if (this.game.viewCredits === true) {
@@ -56,6 +81,8 @@ this.selectsound.src = selectsound
         this.game.viewControls = true
         this.game.viewMainMenu = false
       }
+
+      // For viewing the controls
       if (event.key === 'v' && this.game.gameStart === false) {
         this.selectsound.play()
         if (this.game.viewControls === true) {
@@ -64,6 +91,8 @@ this.selectsound.src = selectsound
         this.game.viewCredits = true
         this.game.viewMainMenu = false
       }
+
+      // For going back to the main menu
       if (event.key === 'b') {
         this.selectsound.play()
         this.game.viewControls = false
@@ -83,6 +112,7 @@ this.selectsound.src = selectsound
       this.mouseY = event.clientY - this.game.canvasPosition.top
     })
 
+    // Player1 shooting
     window.addEventListener('mousedown', (event) => {
       this.shootsound.play()
       this.game.player.setState("shooting");
